8cb0e12df2ddb247234eec3a06395fd11f28c7ee
mick17gh <mike17gh@gmail.com>
2023-12-29 19:27:12 +0000

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          <h1>ShadNinja</h1>
+        </nav>
+        {children}
+        
+        </body>
     </html>
   )
 }
---
+++ app/loading.tsx
@@ -0,0 +1,15 @@
+import SkeletonCard from "@/components/skeleton-card";
+
+const Loading = () => {
+    return (
+        <main>
+        <div className="grid grid-cols-3 gap-8">
+            {"abcdefghi".split('').map(i =>(
+                <SkeletonCard key={i} />
+            ))}
+        </div>
+        </main> 
+     );
+}
+ 
+export default Loading;
